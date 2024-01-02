c2edd0e60c226bc54eb349692e0ffcf2f9a203f4
Sgun369 <souhaiebjebali@gmail.com>
2024-01-02 14:08:38 +0100

    8-main.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usrr/bin/env python3
+"""Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> callable:
+    """returns a function that multiplies a float by multiplier"""
+    def multiply(num: float) -> float:
+        return num * multiplier
+    return multiply
