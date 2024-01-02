8c1261d9a31161742a1b8d1453db473dc8599902
Sgun369 <souhaiebjebali@gmail.com>
2024-01-02 11:11:20 +0100

    2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 """Basic annotations - floor"""
+import math
 
 
 def floor(n: float) -> int:
     """ returns the floor of the float."""
-    return int(n)
+    if isinstance(n, float):
+        n = math.floor(n)
+    return n
