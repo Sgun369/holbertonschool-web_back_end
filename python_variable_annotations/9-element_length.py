8deb8d874d8c0abba5120eeee89dd47bbae37489
Sgun369 <souhaiebjebali@gmail.com>
2024-01-02 14:21:55 +0100

    9-element_length.py

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Let's duck type an iterable object"""
+from typing import List, Tuple, Iterable, Sequence
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """Takes a list of string and returns a list of tuples
+    where each tuple contains an element
+    from the input list and its corresponding length
+    """
+    return [(i, len(i)) for i in lst]
