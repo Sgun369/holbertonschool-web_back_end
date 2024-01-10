2854150c5f169f5045d3a54b6fdbc6293240d237
Sgun369 <souhaiebjebali@gmail.com>
2024-01-10 17:13:41 +0100

    10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -2,7 +2,7 @@
 """ Change school topics"""
 
 
-def update_topiics(mongo_collection, name, topics):
+def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
     return mongo_collection.update_many(
         {"name": name}, {"$set": {"topics": topics}})
