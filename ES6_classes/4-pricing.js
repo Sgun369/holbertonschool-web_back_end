a15ce6caa51e3d34e28cd03b336ea48160b66047
Sgun369 <souhaiebjebali@gmail.com>
2024-01-18 20:04:34 +0100

    4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = amount;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    } else {
+      this._currency = currency;
+    }
+  }
+
+  displayFullPrice() {
+    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
+  }
+
+  static convertPrice(amount, conversioRate) {
+    return (amount * conversionRate);
+  }
+}
